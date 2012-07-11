a8994ff0812a6cd517dc7a0dcb6c13f66c57cc0f
saldin <zhaocn@live.cn>
2012-07-11 22:37:46 +0800

    框架

---
+++ GlacierApplication/GlacierFramework/GlacierFramework/GlacierFramework.h
@@ -0,0 +1,13 @@
+//
+//  GlacierFramework.h
+//  GlacierFramework
+//
+//  Created by chang liang on 12-7-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GlacierFramework : NSObject
+
+@end
