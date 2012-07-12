cfb6e23f61695691420aa8e96c5565e901162225
saldin <zhaocn@live.cn>
2012-07-12 22:48:11 +0800

    fr

---
+++ GlacierFramework/GlacierFramework/BaseController.h
@@ -0,0 +1,12 @@
+//
+//  BaseController.h
+//  GlacierFramework
+//
+//  Created by chang liang on 12-7-12.
+//  Copyright (c) 2012年 Glacier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@interface BaseController : UIViewController
+
+@end
---
+++ GlacierFramework/GlacierFramework/BaseController.m
@@ -0,0 +1,25 @@
+//
+//  BaseController.m
+//  GlacierFramework
+//
+//  Created by chang liang on 12-7-12.
+//  Copyright (c) 2012年 Glacier. All rights reserved.
+//
+
+#import "BaseController.h"
+@interface BaseController ()
+
+@end
+
+@implementation BaseController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
