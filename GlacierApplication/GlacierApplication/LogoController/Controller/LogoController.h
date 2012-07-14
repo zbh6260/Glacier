227fefc3da427e963af9acaf94e0e2752597828a
saldin <zhaocn@live.cn>
2012-07-14 23:46:20 +0800

    去除所有警告

---
+++ GlacierApplication/GlacierApplication/LogoController/Controller/LogoController.h
@@ -0,0 +1,13 @@
+//
+//  LogoController.h
+//  GlacierApplication
+//
+//  Created by chang liang on 12-7-14.
+//  Copyright (c) 2012年 Glacier. All rights reserved.
+//
+
+#import "GlacierController.h"
+
+@interface LogoController : GlacierController
+
+@end
---
+++ GlacierApplication/GlacierApplication/LogoController/Controller/LogoController.m
@@ -0,0 +1,44 @@
+//
+//  LogoController.m
+//  GlacierApplication
+//
+//  Created by chang liang on 12-7-14.
+//  Copyright (c) 2012年 Glacier. All rights reserved.
+//
+
+#import "LogoController.h"
+
+@interface LogoController ()
+
+@end
+
+@implementation LogoController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
